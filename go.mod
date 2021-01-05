0b4f819bc353cec3e61b8e5335394524a2b722d9
Marcus Lee <marcustutorial@hotmail.com>
2021-01-05 13:27:21 +0800

    Added products handlers, marshalling data to JSON and send it as a response

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marcustut/go-microservices
+
+go 1.15
--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"context"
-	"github.com/marcustut/go-microservices/handlers"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/marcustut/go-microservices/handlers"
 )
 
 func main() {
+
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
+
+	// Create the handlers
+	ph := handlers.NewProducts(l)
 	gh := handlers.NewGoodbye(l)
 
+	// Create a new serve mux and register the handlers
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	sm.Handle("/", ph)
 	sm.Handle("/goodbye", gh)
 
 	s := &http.Server{
@@ -27,7 +32,10 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
+	// Create a new server
 	go func() {
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Fatal(err)
